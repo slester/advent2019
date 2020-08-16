1da029a39770d91729269145765ab91ae29210a7
Stephen Lester <slester@startmail.com>
2020-08-16 12:19:42 -0700

    day 3

--- day3.clj
+++ day3.clj
@@ -42,13 +42,22 @@
 (def wire2 (run-commands wire2-commands))
 (defn crosspoints [a b] (intersection (into #{} a) (into #{} b)))
 
+;; part 1
 (defn shortest-distance [a b]
   (manhattan-distance origin
                       (first (sort-by #(manhattan-distance % origin)
                                       (crosspoints a b)))))
-
-;; part 1
 (shortest-distance wire1 wire2)
+
+;; part 2
+(defn shortest-steps [a b]
+  (first
+    (sort
+      (for [i (crosspoints a b)
+            :let [a-steps (.indexOf a i)
+                  b-steps (.indexOf b i)]]
+        (+ 2 a-steps b-steps))))) ;; must add 2 to account for the origin->first point
+(shortest-steps wire1 wire2)
 
 ;; examples
 (= {:x -30 :y 0} (last (generate-points origin "L30")))
@@ -69,3 +78,11 @@
 (def ex3a-commands (split "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" #","))
 (def ex3b-commands (split "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" #","))
 (= 135 (shortest-distance (run-commands ex3a-commands) (run-commands ex3b-commands)))
+
+
+(def ex4a-commands (split "R75,D30,R83,U83,L12,D49,R71,U7,L72" #","))
+(def ex4b-commands (split "U62,R66,U55,R34,D71,R55,D58,R83" #","))
+(= 610 (shortest-steps (run-commands ex4a-commands) (run-commands ex4b-commands)))
+(def ex5a-commands (split "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" #","))
+(def ex5b-commands (split "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" #","))
+(= 410 (shortest-steps (run-commands ex5a-commands) (run-commands ex5b-commands)))
