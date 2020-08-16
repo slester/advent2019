68185e95732ed3faeef06be82b8310a1ccfec9cd
Stephen Lester <slester@startmail.com>
2020-08-15 22:39:45 -0700

    day 3

--- day3.clj
+++ day3.clj
@@ -1,5 +1,6 @@
 (ns day3
-  (:require [clojure.string :refer [split]]))
+  (:require [clojure.set :refer [union intersection]]
+            [clojure.string :refer [split]]))
 
 (def input-lines (split (slurp "input/3.txt") #"\n"))
 (def wire1-directions (into [] (split (first input-lines) #",")))
@@ -16,25 +17,29 @@
         count (read-string (apply str (rest pieces)))]
     [dir count]))
 
-(defn points-cross? [a b]
-  (and (= (:x a) (:x b))
-       (= (:y a) (:y b))))
+(defn generate-points [point command]
+  (into #{} (let [[dir count] (parse-direction command)
+                  [f axis] (case dir "R" [+ :x]
+                                     "L" [- :x]
+                                     "U" [+ :y]
+                                     "D" [- :y])]
+              (for [i (range 1 (inc count))]
+                (update-in point [axis] (partial f i))))))
 
-(defn generate-points [point directions]
-  (into #{} (flatten (for [d directions]
-                       ;; 'point' isn't updating between directions
-                       (let [[dir count] (parse-direction d)
-                             [f axis] (case dir "R" [+ :x]
-                                                "L" [- :x]
-                                                "U" [+ :y]
-                                                "D" [- :y])]
-                         (for [i (range 1 (inc count))]
-                           (update-in point [axis] (partial f i))))))))
+(defn run-commands [commands]
+  (loop [p origin
+         cs commands
+         ps #{}]
+    (if (empty? cs)
+      ps
+      (let [c (first cs)
+            new-points (generate-points p c)
+            new-origin (last new-points)]
+        (recur new-origin (rest cs) (union ps new-points))))))
 
-(def wire1 (generate-points origin wire1-directions))
-(def wire2 (generate-points origin wire2-directions))
-(def crosspoints (for [a wire1
-                       b wire2
-                       :when (points-cross? a b)] a))
 
-(second (sort-by #(manhattan-distance % origin) < crosspoints))
+(def wire1 (run-commands wire1-directions))
+(def wire2 (run-commands wire2-directions))
+(def crosspoints (intersection wire1 wire2))
+
+(println (second (sort-by #(manhattan-distance % origin) < crosspoints)))
